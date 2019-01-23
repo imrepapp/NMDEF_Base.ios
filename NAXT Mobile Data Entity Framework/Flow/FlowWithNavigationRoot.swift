6b408cddd4ba5f0df1573c2ca4e2e24ba430a5ae
Papp Imre <papp.imre.imr@gmail.com>
2019-01-23 15:31:30 +0100

    initial commit

---
+++ NAXT Mobile Data Entity Framework/Flow/FlowWithNavigationRoot.swift
@@ -0,0 +1,60 @@
+//
+// Created by Papp Imre on 2019-01-19.
+// Copyright (c) 2019 XAPT Kft. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import RxFlow
+import Reusable
+
+private var navigationContext: UInt8 = 0
+
+public protocol FlowWithNavigationRoot: Flow, HasRootController where RootControllerType == UINavigationController {
+    func create() -> UINavigationController
+}
+
+public extension FlowWithNavigationRoot where Self: BaseFlow {
+    var root: Presentable {
+        return self.rootViewController
+    }
+
+    func create() -> UINavigationController {
+        return UINavigationController()
+    }
+
+    func instantiate(_ rootFlowItem: NextFlowItem) -> NextFlowItems {
+        return .one(flowItem: rootFlowItem)
+    }
+
+    public var rootViewController: UINavigationController {
+        return self.synchronized {
+            if let nav = objc_getAssociatedObject(self, &navigationContext) as? UINavigationController {
+                return nav
+            }
+            let newNav = self.create()
+            objc_setAssociatedObject(self, &navigationContext, newNav, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            return newNav
+        }
+    }
+
+    public func pushNavigation<TViewController>(to viewControllerClass: TViewController.Type, animated: Bool = true, params: Parameters? = nil) -> NextFlowItems
+            where TViewController: UIViewController & BaseViewControllerProtocol, TViewController.TViewModel: Stepper {
+        let vc = viewControllerClass.instantiate()
+        if (params != nil) {
+            vc.viewModel.instantiate(with: params!)
+        }
+        self.rootViewController.pushViewController(vc, animated: animated)
+        return .one(flowItem: NextFlowItem(nextPresentable: vc, nextStepper: vc.viewModel))
+    }
+}
+
+public extension FlowWithNavigationRoot where Self: BaseFlow & StoryboardSceneBased {
+    func create() -> UINavigationController {
+        let navigationController = Self.sceneStoryboard.instantiateViewController(withIdentifier: Self.sceneIdentifier) as! UINavigationController
+        let vc = UIViewController()
+        vc.view.backgroundColor = UIColor.white
+        navigationController.viewControllers = [vc]
+        return navigationController
+    }
+}
---
+++ NAXT Mobile Data Entity Framework/Model/BaseModel.swift
@@ -0,0 +1,9 @@
+//
+// Created by Papp Imre on 2019-01-22.
+// Copyright (c) 2019 XAPT Kft. All rights reserved.
+//
+
+import Foundation
+
+public protocol BaseModel {
+}
